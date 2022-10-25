590f4bb227ba86dac39043a8ea53e22b9168bd66
Geraldine Bengsch <geraldine_be@hotmail.com>
2022-10-25 17:59:37 +0100

    add second drawing field

--- assets/numbers.js
+++ assets/numbers.js
@@ -88,14 +88,7 @@ function handleClearButtonClick(event) {
 // == Helper Functions ==
 // ======================
 
-window.addEventListener('mousemove', mouseMoveEvent);
 
-function mouseMoveEvent(e) {
-    var canvas = document.getElementById('canvas');
-    var rect = canvas.getBoundingClientRect();
-    var mouseX = e.clientX - rect.left;
-    var mouseY = e.clientY - rect.top;
-};
 
 
 function getMousePositionOnCanvas(event) {
@@ -119,4 +112,130 @@ function getMousePositionOnCanvas(event) {
 
 function clearCanvas() {
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+
+
+
+// ====================
+// == second canvas  ==
+// ====================
+
+
+
+
+// =============
+// == Globals ==
+// =============
+const canvas1 = document.getElementById("drawing-area1");
+const canvasContext1 = canvas1.getContext("2d");
+const clearButton1 = document.getElementById("clear-button1");
+const state1 = {
+  mousedown: false,
+};
+
+// ===================
+// == Configuration ==
+// ===================
+const lineWidth1 = 10;
+const halfLineWidth1 = lineWidth1 / 2;
+const fillStyle1 = "#333";
+const strokeStyle1 = "#333";
+const shadowColor1 = "#333";
+const shadowBlur1 = lineWidth1 / 0;
+
+// =====================
+// == Event Listeners ==
+// =====================
+canvas1.addEventListener("mousedown", handleWritingStart1);
+canvas1.addEventListener("mousemove", handleWritingInProgress1);
+canvas1.addEventListener("mouseup", handleDrawingEnd1);
+canvas1.addEventListener("mouseout", handleDrawingEnd1);
+
+canvas1.addEventListener("touchstart", handleWritingStart1);
+canvas1.addEventListener("touchmove", handleWritingInProgress1);
+canvas1.addEventListener("touchend", handleDrawingEnd1);
+
+clearButton1.addEventListener("click", handleClearButtonClick1);
+
+// ====================
+// == Event Handlers ==
+// ====================
+function handleWritingStart1(event) {
+  event.preventDefault();
+
+  const mousePos1 = getMousePositionOnCanvas1(event);
+
+  canvasContext1.beginPath();
+
+  canvasContext1.moveTo(mousePos1.x, mousePos1.y);
+
+  canvasContext1.lineWidth1 = lineWidth1;
+  canvasContext1.strokeStyle1 = strokeStyle1;
+  canvasContext1.shadowColor1 = null;
+  canvasContext1.shadowBlur1 = null;
+
+  canvasContext1.fill();
+
+  state1.mousedown = true;
+}
+
+function handleWritingInProgress1(event) {
+  event.preventDefault();
+
+  if (state1.mousedown) {
+    const mousePos1 = getMousePositionOnCanvas1(event);
+
+    canvasContext1.lineTo(mousePos1.x, mousePos1.y);
+    canvasContext1.stroke();
+  }
+}
+
+function handleDrawingEnd1(event) {
+  event.preventDefault();
+
+  if (state1.mousedown) {
+    canvasContext1.shadowColor1 = shadowColor1;
+    canvasContext1.shadowBlur1 = shadowBlur1;
+
+    canvasContext1.stroke();
+  }
+
+  state1.mousedown = false;
+}
+
+function handleClearButtonClick1(event) {
+  event.preventDefault();
+
+  clearCanvas1();
+}
+
+// ======================
+// == Helper Functions ==
+// ======================
+
+
+
+
+function getMousePositionOnCanvas1(event) {
+ 
+  var rect1 = canvas1.getBoundingClientRect();
+  const clientX = (event.clientX-rect1.left ) / (rect1.right - rect1.left) * canvas1.width || (event.touches[0].clientX - rect1.left) / (rect1.right - rect1.left) * canvas1.width;
+  const clientY = (event.clientY-rect1.top) / (rect1.bottom - rect1.top) * canvas1.height || (event.touches[0].clientY - rect1.top) / (rect1.bottom - rect1.top) * canvas1.height;
+  const { offsetLeft, offsetTop } = event.target;
+  const canvasX = clientX 
+  const canvasY = clientY 
+
+  return { x: canvasX, y: canvasY };
+}
+
+
+
+
+
+
+
+
+function clearCanvas1() {
+  canvasContext1.clearRect(0, 0, canvas1.width, canvas1.height);
 }
